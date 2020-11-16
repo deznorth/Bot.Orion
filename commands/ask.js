5282a6f379e7167d75491e87d152fec4a03e2dcd
David Rojas <deznorth@gmail.com>
2020-11-16 01:58:45 -0500

    Expand ask.js functionality

--- commands/ask.js
+++ commands/ask.js
@@ -1,63 +1,151 @@
 const log = require('debug')('orion:ask');
-const { MessageEmbed } = require('discord.js');
-const { colors } =require('../botconfig.json');
+const Discord = require('discord.js');
+const { colors, reactions } =require('../botconfig.json');
 
-const buildEmbed = (author, question, expiration) => {
-  return new MessageEmbed()
-    .setColor(colors.blue)
-    .setTitle(`"${question}"`)
-    .addField('Asked by', author, true)
-    .addField('Expires by', expiration.toLocaleTimeString('en-US'), true)
-    .setFooter('Only thumbs up/down will be counted. Totals will not include my votes.');
+const buildQuestionEmbed = (author, options) => {
+
+  const {
+    question,
+    expiration,
+    includeMaybe,
+    passMinimum,
+  } = options;
+
+  const allowedReactions = includeMaybe ?
+  `${reactions.yes}, ${reactions.no} and ${reactions.maybe}` :
+  `${reactions.yes} and ${reactions.no}`;
+
+  const embed = new Discord.MessageEmbed()
+  .setColor(colors.blue)
+  .setTitle(question)
+  .addField('Asked by', author, true)
+  .addField('Expires by', expiration.toLocaleTimeString('en-US'), true)
+  .setFooter(`Only "${allowedReactions}" will be counted. Totals will not include my votes.`);
+
+  if (passMinimum) embed.addField('Required Votes', passMinimum, true);
+
+  return embed;
 };
 
-const reactionFilter = reaction => {
-  return ['👍', '👎'].includes(reaction.emoji.name);
+const buildResultsEmbed = (questionEmbed, options, results) => {
+
+  const {
+    includeMaybe,
+    passMinimum,
+  } = options;
+
+  const {
+    yesResults,
+    noResults,
+    maybeResults,
+  } = results;
+
+  let resultColor = colors.orange; // default value for ties or maybes
+
+  const resultColors = [
+    { ...yesResults, color: colors.green },
+    { ... noResults, color: colors.red },
+    { ...maybeResults, color: colors.grey },
+  ];
+
+  resultColors.sort((a, b) => {
+    if (a.votes === b.votes) return 0;
+    return a.votes > b.votes ? -1 : 1;
+  })
+
+  const isTie = (yesResults.votes !== 0 && yesResults.votes === noResults.votes) ||
+  (yesResults.votes !== 0 && yesResults.votes === maybeResults.votes) ||
+  (noResults.votes !== 0 && noResults.votes === maybeResults.votes);
+
+  if (!isTie) resultColor = resultColors[0].color;
+
+  if (passMinimum && passMinimum > yesResults.votes) resultColor = colors.red;
+
+  const embed = new Discord.MessageEmbed(questionEmbed)
+  .setColor(resultColor);
+
+  if(!passMinimum) embed.addField('\u200b','\u200b',true);
+
+  embed.addField(reactions.yes, appendVotersToResult(yesResults), true)
+  .addField(reactions.no, appendVotersToResult(noResults), true);
+
+  if (includeMaybe) embed.addField(reactions.maybe, appendVotersToResult(maybeResults), true);
+
+  return embed;
+};
+
+const appendVotersToResult = ({ votes, voters }) => {
+  const votersString = voters.map(voter => `${voter}\n`);
+  return `${votes}\n${votersString}`;
+};
+
+const getOptions = (args) => {
+  const options = {
+    question: args[0],
+    duration: parseFloat(args[1]) * 60 * 1000, // convert min to ms
+  };
+
+  options.expiration = new Date(Date.now() + options.duration);
+  options.includeMaybe = args[2] ? new Boolean(args[2]) : false;
+  options.passMinimum = args[3] ? parseInt(args[3]) : null;
+
+  return options;
+};
+
+const reactionFilter = reaction => [reactions.yes, reactions.no, reactions.maybe].includes(reaction.emoji.name);
+
+const getReactionResults = async (reactionCollection, filterReaction) => {
+  const reaction = reactionCollection.filter(reaction => reaction.emoji.name === filterReaction).first();
+  const votes = reaction.count - 1;
+  const users = await reaction.users.fetch();
+
+  const voters = users.filter(u => !u.bot);
+
+  return {
+    votes,
+    voters,
+  };
 };
 
 const exec = async (message, args) => {
-  const author = message.author;
 
-  const duration = args.shift() * 60 * 1000; // convert min to ms
-  const expiration = new Date(Date.now() + duration);
-  const question = args.join(" ");
+  const options = getOptions(args);
+
+  const author = message.author;
   
-  const embed = buildEmbed(author, question, expiration);
+  const questionEmbed = buildQuestionEmbed(author, options);
   
-  const poll = await message.channel.send(embed);
+  const questionMessage = await message.channel.send(questionEmbed);
 
   if (message.channel.type !== 'dm') message.delete();
 
-  poll.react('👍');
-  poll.react('👎');
+  questionMessage.react(reactions.yes);
+  questionMessage.react(reactions.no);
+
+  if (options.includeMaybe) questionMessage.react(reactions.maybe);
   
-  await poll.awaitReactions(reactionFilter, { time: duration })
-  .then(collected => {
-    const upvotes = collected.filter(reaction => reaction.emoji.name === '👍').first().count - 1;
-    const downvotes = collected.filter(reaction => reaction.emoji.name === '👎').first().count - 1;
+  await questionMessage.awaitReactions(reactionFilter, { time: options.duration })
+  .then(async collected => {
+    const yesResults = await getReactionResults(collected, reactions.yes);
+    const noResults = await getReactionResults(collected, reactions.no);
+    const maybeResults = await getReactionResults(collected, reactions.maybe);
 
-    let resultColor = colors.orange;
+    const resultsEmbed = buildResultsEmbed(questionEmbed, options, {
+      yesResults,
+      noResults,
+      maybeResults,
+    });
 
-    if (upvotes !== downvotes) {
-      resultColor = upvotes > downvotes ? colors.green : colors.red;
-    }
-
-    const resultsEmbed = new MessageEmbed(embed)
-    .setColor(resultColor)
-    .addField('\u200b','\u200b',true)
-    .addField('👍', upvotes, true)
-    .addField('👎', downvotes, true);
-
-    poll.edit(resultsEmbed);
+    questionMessage.edit(resultsEmbed);
   });
 };
 
 module.exports = {
   name: 'ask',
   description: 'Posts a question, collects answers and shows results after a given time limit.',
-  usage: '<duration in minutes> <yes/no question>',
-  example: '10 does anyone want to play rocket league?',
+  usage: '"<question>" <duration in minutes> <allow "maybe" option> <minimum votes to pass>',
+  example: '"does anyone want to play rocket league?" 10 true 3',
   argsRequired: true,
-  // guildOnly: true, UNCOMMENT WHEN FINALIZING
+  guildOnly: !(process.env.NODE_ENV === 'dev'), // Enable DM testing while in development
   exec,
 };
