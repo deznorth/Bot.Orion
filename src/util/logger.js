c95eb2de456251fce8262b3b825de70e14422066
David Rojas <deznorth@gmail.com>
2021-03-14 02:40:52 -0400

    fix logger

--- src/util/logger.js
+++ src/util/logger.js
@@ -1,4 +1,25 @@
-const isDev = require('./constants').isDev;
-const logger = !isDev ? require('debug-timezone') : require('debug');
 
-module.exports = namespace => !isDev ? logger(namespace, 'MM/DD/YY h:mm:ss A z', 'America/New_York') : logger(namespace);
+const moment = require('moment-timezone');
+const debug = require('debug');
+
+const logger = namespace => {
+  debug.formatArgs = function (args) {
+    const {namespace: name, useColors} = this;
+    const timestamp = `[${moment().tz('America/New_York').format('MM-DD-YYYY h:mm:ss a z')}]`;
+
+    if (useColors) {
+      const c = this.color;
+      const colorCode = '\u001B[3' + (c < 8 ? c : '8;5;' + c);
+      const prefix = `  ${colorCode};1m${name} \u001B[0m`;
+
+      args[0] = timestamp + prefix + args[0].split('\n').join('\n' + prefix);
+      args.push(colorCode + 'm+' + debug.humanize(this.diff) + '\u001B[0m');
+    } else {
+      args[0] = timestamp + name + ' ' + args[0];
+    }
+  }
+
+  return debug(namespace);
+}
+
+module.exports = logger;
